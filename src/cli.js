9f3703a33d4e01aff4c808a4d898bbf991cf68b7
Anton Smolko <asmolko@yandex.ru>
2020-10-30 23:24:37 +0300

    update: cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import promptly from 'promptly';
 
 export default async () => {
-  console.log('May I have you name?');
-  // await console.log('Welcome to the Brain Games!');
-  // const answer = await promptly.prompt('May I have you name?');
+  // console.log('Welcome to the Brain Games!');
+  promptly.prompt('May I have you name?');
   // await console.log('Hello,', answer);
 };
